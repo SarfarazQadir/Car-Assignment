35e4b25c4e333192316a5bba53d0bbc0161f663a
Sarfaraz Qadir <sarfarazqadir594@gmail.com>
2024-11-13 20:21:09 +0500

    Add Sounds

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-gsap.to(".box", { 
-    x: -1400,
-    duration: 3,
-    ease: "none",
-}).repeat(Infinity);
-
-// var red = document.getElementById("red");
-// var green = document.getElementById("green");
-// var yellow = document.getElementById("yellow");
-
 // function red(){
-//    var a = red.style= "visible";   
-//    var b = green.style= "visible";   
-//    var c = yellow.style= "visible";   
-//     if(red.style == "visible"){
-//         b.style = "hidden";
+    //    var a = red.style= "visible";   
+    //    var b = green.style= "visible";   
+    //    var c = yellow.style= "visible";   
+    //     if(red.style == "visible"){
+        //         b.style = "hidden";
 //         c.style = "hidden";
 //     }else if(b == "visible"){
-
+    
 //     }
 // }
 
-function signal(params) {
-var a = document.getElementById(params)
-if(params == "red"){
-    a.style = "visible";
-}else if(params == "green"){
-    a.style = "visible"
+var car = document.getElementById("car");
+var cargoing = document.getElementById("cargoing");
+var start = document.getElementById("start");
+
+function red() {
+    gsap.killTweensOf(car)
+    var red = document.getElementById("red");
+    red.style.backgroundColor="red"
 }    
-else if(params == "yellow"){
-    a.style = "visible"
-}
+function green() {
+    // gsap.to(".box", { 
+    //     x: -1400,
+    //     duration: 3,
+    //     ease: "none",
+    // }).repeat(Infinity);
+    gsap.fromTo( ".box",{ x: 0, }, { x: -1400, duration: 3}).repeat(Infinity);
+    cargoing.play();
+    var green = document.getElementById("green");
+    green.style.backgroundColor="green"
+}    
+function yellow() {
+    start.play();
+    var yellow = document.getElementById("yellow");
+    yellow.style.backgroundColor= "yellow"
 }    
